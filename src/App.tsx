3c467fc033caa95e14deafa2b85f474520713b55
Ahmed Samir <ahmedsamir7685@gmail.com>
2026-02-22 22:16:36 +0200

    finishing the task

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { Route, Routes } from "react-router-dom";
+import OnlineConsultation from "./pages/online_consultation/page.tsx";
+import WaitingRoom from "./pages/online_consultation/Components/waiting_room/page.tsx";
+import VideoCall from "./pages/online_consultation/videoCall/page.tsx";
+
 
 const App = () => {
-  return <div>App</div>;
+    return (
+        <div>
+            <Routes>
+                <Route path="/" element={<OnlineConsultation />} />
+                <Route path="/waiting_room" element={<WaitingRoom />} />
+                <Route path="/video_call" element={<VideoCall />} />
+            </Routes>
+        </div>
+    );
 };
 
 export default App;
---
+++ src/pages/online_consultation/page.tsx
@@ -0,0 +1,58 @@
+import DoctorAvailableOnline from "./Components/doctor_available_online";
+import Button from "./Components/button";
+export default function onlineConsultation() {
+    return (
+        <main className="lg:px-24 md:px-16 px-5">
+            <h2 className="text-[#097178] lg:text-3xl font-bold text-xl">
+                Doctors Available Online
+            </h2>
+            <div className="flex justify-between items-center gap-5 flex-wrap my-4">
+                <div className="md:flex grid grid-cols-2 gap-3">
+                    <Button text="Filter by Specialty" />
+                    <Button
+                        text="Filter by Location"
+                        DropdownMenuItemText={[
+                            "Egypt",
+                            "United Arab Emirates",
+                            "Kuwait",
+                            "Qatar",
+                            "Bahrain",
+                            "Oman",
+                        ]}
+                    />
+                    <Button
+                        text="Availability"
+                        DropdownMenuItemText={["Available", "Not Available"]}
+                    />
+                </div>
+                <Button
+                    text="Filter by Specialty"
+                    DropdownMenuItemText={[
+                        "Nearest",
+                        "Cheapest",
+                        "Highest Rated",
+                        "Most Experienced",
+                        "Earliest Available",
+                        "Female Doctor",
+                        "Male Doctor",
+                    ]}
+                />
+            </div>
+            <div className="flex flex-col w-full gap-5 items-stretch justify-center">
+                {Array.from({ length: 6 }).map((_, index) => {
+                    return (
+                        <DoctorAvailableOnline
+                            key={index}
+                            image="image.png"
+                            name="Dr. Sarah Ahmed"
+                            rate="4.5"
+                            specialist="Dermatologist"
+                            location="New York, NY"
+                            exp="15"
+                        />
+                    );
+                })}
+            </div>
+        </main>
+    );
+}
